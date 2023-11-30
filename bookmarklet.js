82dfc124c8212d6cf47ac455ac2931e6d9f69e6b
forveined1 <88626626+forveined1@users.noreply.github.com>
2023-11-29 21:56:19 -0500

    Update bookmarklet.js

--- bookmarklet.js
+++ bookmarklet.js
@@ -1,1 +1,5 @@
-fetch("https://raw.githubusercontent.com/forveined1/retro-hax/main/main.js).then(r => r.text()).then(r => eval(r))
+fetch("https://raw.githubusercontent.com/forveined1/retro-hax/main/main.js")
+  .then(response => response.text())
+  .then(script => eval(script))
+// Error Handling
+  .catch(error => console.error("Error fetching or evaluating script:", error));
